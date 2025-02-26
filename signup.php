d8112fb74451d17777accc3df97cf2039de0ec31
Marvin <natnatmarvin@gmail.com>
2025-02-26 21:43:13 +0800

    udates

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title> Login Form </title>
+        <title>Signup Form </title>
 
         <!-- CSS -->
         <link rel="stylesheet" href="assets/css/style.css">
@@ -18,41 +18,20 @@
         <section class="container forms">
             <div class="form">
                 <div class="form-content">
-                    <header>Login</header>
-                    <form action="#">
-                        <div class="field input-field">
-                            <input type="email" placeholder="Email" class="input">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Password" class="password">
-                            <i class='bx bx-hide eye-icon'></i>
-                        </div>
-
-                        <!-- <div class="form-link">
-                            <a href="#" class="forgot-pass">Forgot password?</a>
-                        </div> -->
-
-                        <div class="field button-field">
-                            <button>Login</button>
-                        </div>
-                    </form>
-
-                    <div class="form-link">
-                        <span>Don't have an account? <a href="#">Signup</a></span>
-                    </div>
-                </div>
-
-
-
-            </div>
-
-            <!-- Signup Form -->
-
-            <div class="form signup">
-                <div class="form-content">
                     <header>Signup</header>
                     <form action="#">
+                        <div class="field input-field">
+                            <input type="text" placeholder="First Name" class="input">
+                        </div>
+
+                        <div class="field input-field">
+                            <input type="text" placeholder="Last Name" class="input">
+                        </div>
+
+                        <div class="field input-field">
+                            <input type="text" placeholder="Address" class="input">
+                        </div>
+
                         <div class="field input-field">
                             <input type="email" placeholder="Email" class="input">
                         </div>
@@ -63,7 +42,7 @@
 
                         <div class="field input-field">
                             <input type="password" placeholder="Confirm password" class="password">
-                            <i class='bx bx-hide eye-icon'></i>
+                            <!-- <i class='bx bx-hide eye-icon'></i> -->
                         </div>
 
                         <div class="field button-field">
