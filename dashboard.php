6890ccac108599ffc0f59504ccb1f4adf26c9e81
Marvin <natnatmarvin@gmail.com>
2025-02-26 22:16:13 +0800

    updates

---
+++ dashboard.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!-- Coding by CodingLab | www.codinglabweb.com-->
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Login Form </title>
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+                
+        <!-- Boxicons CSS -->
+        <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+                        
+    </head>
+    <body>
+       
+        <script src="js/script.js"></script>
+    </body>
+</html>
--- login.php
+++ login.php
@@ -42,40 +42,6 @@
                         <span>Don't have an account? <a href="#">Signup</a></span>
                     </div>
                 </div>
-
-
-
-            </div>
-
-            <!-- Signup Form -->
-
-            <div class="form signup">
-                <div class="form-content">
-                    <header>Signup</header>
-                    <form action="#">
-                        <div class="field input-field">
-                            <input type="email" placeholder="Email" class="input">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Create password" class="password">
-                        </div>
-
-                        <div class="field input-field">
-                            <input type="password" placeholder="Confirm password" class="password">
-                            <i class='bx bx-hide eye-icon'></i>
-                        </div>
-
-                        <div class="field button-field">
-                            <button>Signup</button>
-                        </div>
-                    </form>
-
-                    <div class="form-link">
-                        <span>Already have an account? <a href="#" class="link login-link">Login</a></span>
-                    </div>
-                </div>
-
             </div>
         </section>
 
